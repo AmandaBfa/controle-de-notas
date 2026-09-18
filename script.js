dcac3320aa07a6ec5d95fc64b4e919e4c8cb7bfc
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2026-09-18 15:24:53 -0300

    feat: adiciona calculo e exibicao da media

---
+++ script.js
@@ -0,0 +1,8 @@
+function calcularMedia() {
+    const nota1 = parseFloat(document.getElementById("nota1").value);
+    const nota2 = parseFloat(document.getElementById("nota2").value);
+
+    const media = (nota1 + nota2) / 2;
+
+    document.getElementById("media").textContent = media.toFixed(1);
+}
